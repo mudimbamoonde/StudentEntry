da2f5b6a625fc3017ce07260ce1208b6309c30d6
Mudimba Moonde <mudimbamoonde@gmail.com>
2020-07-19 22:01:24 +0200

    New Commands

--- app/src/main/java/com/maamba/studententry/AddLearner.java
+++ app/src/main/java/com/maamba/studententry/AddLearner.java
@@ -8,19 +8,12 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 public class AddLearner extends AppCompatActivity {
-    ImageView addStudent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_learner);
 
-        addStudent = findViewById(R.id.addStudent);
 
-        addStudent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(AddLearner.this, "Hello", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
--- app/src/main/java/com/maamba/studententry/MainActivity.java
+++ app/src/main/java/com/maamba/studententry/MainActivity.java
@@ -3,12 +3,26 @@ package com.maamba.studententry;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button addStudent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addStudent = findViewById(R.id.addStudent);
+
+        addStudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
