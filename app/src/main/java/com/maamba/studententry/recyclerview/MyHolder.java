1cb98f583f881fb5832b79d552f61285869b933b
Mudimba Moonde <mudimbamoonde@gmail.com>
2020-07-20 09:52:33 +0200

    Recycler View

--- app/src/main/java/com/maamba/studententry/recyclerview/MyHolder.java
+++ app/src/main/java/com/maamba/studententry/recyclerview/MyHolder.java
@@ -1,13 +1,20 @@
 package com.maamba.studententry.recyclerview;
 
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class MyHolder extends RecyclerView.ViewHolder {
+import com.maamba.studententry.R;
 
-    public MyHolder(@NonNull View itemView) {
-        super(itemView);
+public class MyHolder extends RecyclerView.ViewHolder {
+    TextView studentName,yearDate;
+
+    public MyHolder(@NonNull View view) {
+        super(view);
+
+         studentName = view.findViewById(R.id.studentName);
+         yearDate = view.findViewById(R.id.yeardate);
     }
 }
--- app/src/main/java/com/maamba/studententry/recyclerview/myAdapter.java
+++ app/src/main/java/com/maamba/studententry/recyclerview/myAdapter.java
@@ -1,11 +1,15 @@
 package com.maamba.studententry.recyclerview;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.maamba.studententry.R;
 import com.maamba.studententry.StudentModel;
 
 import java.util.ArrayList;
@@ -13,15 +17,24 @@ import java.util.ArrayList;
 public class myAdapter extends RecyclerView.Adapter<MyHolder> {
     Context context;
     ArrayList<StudentModel> model;
+
+    public myAdapter(Context context, ArrayList<StudentModel> model) {
+        this.context = context;
+        this.model = model;
+    }
+
     @NonNull
     @Override
     public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.rows,parent,false);
+        return new MyHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyHolder holder, int position) {
-
+        String fullName = model.get(position).getFirstName() +" "+ model.get(position).getLastName();
+        holder.studentName.setText(fullName);
+        holder.yearDate.setText(model.get(position).getYearOfEntry());
     }
 
     @Override
