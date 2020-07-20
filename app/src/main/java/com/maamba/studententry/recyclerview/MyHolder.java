66e629897362620401f835a00a921d462174d6e0
Mudimba Moonde <mudimbamoonde@gmail.com>
2020-07-20 09:18:35 +0200

    Recycler View

--- app/src/main/java/com/maamba/studententry/recyclerview/MyHolder.java
+++ app/src/main/java/com/maamba/studententry/recyclerview/MyHolder.java
@@ -6,6 +6,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class MyHolder extends RecyclerView.ViewHolder {
+
     public MyHolder(@NonNull View itemView) {
         super(itemView);
     }
--- app/src/main/java/com/maamba/studententry/recyclerview/myAdapter.java
+++ app/src/main/java/com/maamba/studententry/recyclerview/myAdapter.java
@@ -1,12 +1,18 @@
 package com.maamba.studententry.recyclerview;
 
+import android.content.Context;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class myAdapter extends RecyclerView.Adapter<MyHolder> {
+import com.maamba.studententry.StudentModel;
 
+import java.util.ArrayList;
+
+public class myAdapter extends RecyclerView.Adapter<MyHolder> {
+    Context context;
+    ArrayList<StudentModel> model;
     @NonNull
     @Override
     public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
