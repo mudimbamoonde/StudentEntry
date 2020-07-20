a63e5ae6d4bf4b1c34b1a7645b408af19b7a703b
Mudimba Moonde <mudimbamoonde@gmail.com>
2020-07-20 10:57:39 +0200

    Table Layout

---
+++ app/src/main/java/com/maamba/studententry/DetailedStudent.java
@@ -0,0 +1,14 @@
+package com.maamba.studententry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailedStudent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detailed_student);
+    }
+}
